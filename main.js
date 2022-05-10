c40576854317e286694112486982c5ce98907ed3
Márcio Matos <marciomatos1096@gmail.com>
2022-05-10 19:24:49 -0300

    Finalizações

--- main.js
+++ main.js
@@ -4,7 +4,62 @@ onScroll()
 function onScroll() {
  showNavOnScroll()
  showBackToTopButtonOnScroll()
+
+ activateMenuAtCurrentSection(home)
+ activateMenuAtCurrentSection(services)
+ activateMenuAtCurrentSection(about)
+ activateMenuAtCurrentSection(contact)
 }
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  // o topo da seção
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.offsetHeight
+  
+  // o topo da seção chegou ou ultrapassou a linha alto
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // informações dos dados e da lógica 
+  console.log( 'O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetline)
+
+
+  //verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
+
+
+  // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+
+  console.log('O fundo da seção passou da linha?', sectionEndPassedTargetline)
+
+
+  // limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document 
+  .querySelector(`.menu a[href*=${sectionId}]`)
+
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+
+}
+
 
 function showNavOnScroll(){
   if (scrollY > 0){
@@ -30,18 +85,18 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
-ScrollReveal({
+ScrollReveal() .reveal (`
+#home,
+#home img,
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content
+
   origin: 'top',
   distance: '30px',
   duration: 700,
-}).reveal(`
-  #home,
-  #home img,
-  #home .stats,
-  #services,
-  #services header,
-  #services .card,
-  #about,
-  #about header,
-  #about .content
-  `);
+  `)
